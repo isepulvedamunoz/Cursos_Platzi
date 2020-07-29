a75a593a518facf4a1219b9968db892a4f1d6a73
Ignacio Sepulveda <isepulvedamunoz@gmail.com>
2020-07-29 18:56:53 -0500

    I make the Python's folder, with some python's files, of the first course, of School of Datascience.

---
+++ Datascience school/Python/bucles.py
@@ -0,0 +1,8 @@
+estudiantes = {
+    'mexico' : 10,
+    'colombia' : 8,
+    'chile' : 5
+}
+
+for pais in estudiantes:
+    print(pais)
---
+++ Datascience school/Python/hola_mundo.py
@@ -0,0 +1,11 @@
+nombre = input("¿Cual es tu nombre?: ")
+
+def nombre(nombre):
+
+    largo = length(nombre)
+    respuesta = "hola"
+
+    return respuesta
+
+print(nombre)
+
