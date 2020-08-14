66673eddb4a7aeea307d3e2c0817317c76e4a623
Ignacio Sepulveda <isepulvedamunoz@gmail.com>
2020-08-13 22:24:10 -0500

    Se agregan archivos del curso POO, y se finaliza el curso. ademas se crea carpeta de escuela de negocios

---
+++ Datascience school/Python_POO/ejemplo.py
@@ -0,0 +1,10 @@
+def my_func(x):
+    respuesta = 0
+    for i in range(20):
+        respuesta += 1
+        print(respuesta)
+    return respuesta
+
+if __name__ == '__name__':
+
+    print(my_func(0))
