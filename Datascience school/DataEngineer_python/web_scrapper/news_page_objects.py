46ac52c6b5afbe00f386b29a1cb2923f8b5223cc
Ignacio Sepulveda <isepulvedamunoz@gmail.com>
2020-09-28 00:29:30 -0500

    iniciando curso Data Engineer

---
+++ Datascience school/DataEngineer_python/web_scrapper/news_page_objects.py
@@ -0,0 +1,20 @@
+import requests
+import bs4
+from common import config
+
+
+class HomePage:
+
+    def __init__(self, news_site:uid, url):
+        self._config = config()['news_sites'][news_site_uid]
+        self._queries = self._config['queries']
+        self._html = None
+
+        self._visit(url)
+
+    def _visit(self, url):
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
